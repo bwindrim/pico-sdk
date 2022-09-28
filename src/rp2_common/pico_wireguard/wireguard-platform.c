baf21a2fb2a6bc12eb7bb369fbaf04f4fb002fbf
Brian Windrim <bwindrim@gmail.com>
2022-09-28 15:53:03 +0100

    Updating wireguard-platform.c with RP2-specific random number generation

--- src/rp2_common/pico_wireguard/wireguard-platform.c
+++ src/rp2_common/pico_wireguard/wireguard-platform.c
@@ -5,24 +5,40 @@
 #include "lwip/sys.h"
 #include "lwip/init.h"
 #include "pico/time.h"
+#include "pico.h"
+#include "hardware/structs/rosc.h"
 
-// This file contains a sample Wireguard platform integration
+// This file contains a Wireguard platform integration for Raspberry Pi Pico/Pico-W
 
-// DO NOT USE THIS FUNCTION - IMPLEMENT A BETTER RANDOM BYTE GENERATOR IN YOUR IMPLEMENTATION
+// Stolen from pico_lwip/random.c, as we can't link to it - BW
+static uint8_t pico_lwip_random_byte(int cycles) {
+    static uint8_t byte;
+    assert(cycles >= 8);
+    assert(rosc_hw->status & ROSC_STATUS_ENABLED_BITS);
+    for(int i=0;i<cycles;i++) {
+        // picked a fairly arbitrary polynomial of 0x35u - this doesn't have to be crazily uniform.
+        byte = ((byte << 1) | rosc_hw->randombit) ^ (byte & 0x80u ? 0x35u : 0);
+        // delay a little because the random bit is a little slow
+        busy_wait_at_least_cycles(30);
+    }
+    return byte;
+}
+
+// We trust kilograham's random numbers to be better than rand() - BW
 void wireguard_random_bytes(void *bytes, size_t size) {
 	int x;
 	uint8_t *out = (uint8_t *)bytes;
 	for (x=0; x < size; x++) {
-		out[x] = rand() % 0xFF;
+		out[x] = pico_lwip_random_byte(32);
 	}
 }
 
 uint32_t wireguard_sys_now() {
-	// Default to the LwIP system time
-	return sys_now();
+	// This is what the LwIP system time defaults to - BW
+	return to_ms_since_boot(get_absolute_time());
 }
 
-// CHANGE THIS TO GET THE ACTUAL UNIX TIMESTMP IN MILLIS - HANDSHAKES WILL FAIL IF THIS DOESN'T INCREASE EACH TIME CALLED
+// Get the system time in nanoseconds - HANDSHAKES WILL FAIL IF THIS DOESN'T INCREASE EACH TIME CALLED
 void wireguard_tai64n_now(uint8_t *output) {
 	// See https://cr.yp.to/libtai/tai64.html
 	// 64 bit seconds from 1970 = 8 bytes
