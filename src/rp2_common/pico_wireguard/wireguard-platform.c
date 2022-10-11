505ec93af4f12edf5a7a804aaa8dd1ace355dd7f
Brian Windrim <bwindrim@gmail.com>
2022-10-11 10:30:12 +0100

    Added first version of datetime_to_seconds()

--- src/rp2_common/pico_wireguard/wireguard-platform.c
+++ src/rp2_common/pico_wireguard/wireguard-platform.c
@@ -7,8 +7,10 @@
 #include "pico/time.h"
 #include "pico.h"
 #include "hardware/structs/rosc.h"
+#include "hardware/rtc.h"
+#include "pico/util/datetime.h"
 
-// This file contains a Wireguard platform integration for Raspberry Pi Pico/Pico-W
+// This file contains a Wireguard platform integration for Raspberry Pi Pico-W
 
 // Stolen from pico_lwip/random.c, as we can't link to it - BW
 static uint8_t pico_lwip_random_byte(int cycles) {
@@ -38,12 +40,53 @@ uint32_t wireguard_sys_now() {
 	return to_ms_since_boot(get_absolute_time());
 }
 
+// Convert a datetime_t from the Pico RTC into a roughly TAI64-compatible value of
+// seconds since 1970. We don't need this to be strictly correct (which woud be needlessly
+// complicated) but just to:
+// - always produce the same result for the same value of datetime_t
+// - always produce a larger value for later values of datatime_t
+// These two properties are sufficient to give the "monotonically increasing" behaviour
+// that is required for WireGuard handshake timestamps. This property will be maintained
+// across power-off restarts, provided that the RTC remains correct (but that is not our
+// responsibility here).
+static uint64_t datetime_to_seconds(datetime_t *pDateTime) {
+	static const uint16_t vDaysBefore[12] = {
+		0,   // Jan
+		31,  // Feb
+		60,  // Mar, assume leap year
+		91,  // Apr
+		121, // May
+		152, // Jun
+		182, // Jul
+		213, // Aug
+		244, // Sep
+		274, // Oct
+		305, // Nov
+		335, // Dec
+	};
+	const uint16_t years = pDateTime->year - 1970;
+	const uint64_t days = 366*years + vDaysBefore[pDateTime->month - 1] + pDateTime->day - 1;
+	const uint64_t seconds = ((((days * 24) + pDateTime->hour) * 60) + pDateTime->min) * 60 + pDateTime->sec;
+	return seconds;
+}
+
 // Get the system time in nanoseconds - HANDSHAKES WILL FAIL IF THIS DOESN'T INCREASE EACH TIME CALLED
 void wireguard_tai64n_now(uint8_t *output) {
 	// See https://cr.yp.to/libtai/tai64.html
 	// 64 bit seconds from 1970 = 8 bytes
 	// 32 bit nano seconds from current second
 
+	if (rtc_running()) {
+	    datetime_t t;
+		uint32_t nanos = 0;
+
+        rtc_get_datetime(&t);
+		const uint64_t seconds = datetime_to_seconds(&t);
+
+		U64TO8_BIG(output + 0, seconds);
+		U32TO8_BIG(output + 8, nanos);
+		return;
+	}
 	static uint64_t time_save __attribute__((section(".uninitialized_data"))); // non-resetting time count
 	static uint64_t prev_now = 0ULL;
 	uint64_t now = to_us_since_boot(get_absolute_time()); // microseconds since boot
